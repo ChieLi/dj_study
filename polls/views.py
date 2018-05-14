ea1fc55c1573b5f0ec9485c4d8e88a2115cf2569
liqianhui <liqianhui@ledongli.me>
2018-05-14 22:22:29 +0800

    upload file

--- polls/views.py
+++ polls/views.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from .models import Question
-# from django.template import loader
+from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+# from django.http import Http404
 
 
 def index(request):
@@ -32,3 +32,20 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s", question_id)
+
+
+def upload_file(request):
+    # 请求方法为POST时，进行处理
+    if request.method == "POST":
+        # 获取上传文件，如果没有文件，则默认为None
+        file = request.FILES.get("myfile", None)
+        if file is None:
+            return HttpResponse("no files for upload!")
+        else:
+            # 打开特定的文件进行二进制的写操作
+            with open("/tmp/%s" % file.name, 'wb+') as f:
+                for chunk in file.chunks():
+                    f.write(chunk)
+            return HttpResponse("upload over!")
+    else:
+        return render(request, 'upload_file.html')
